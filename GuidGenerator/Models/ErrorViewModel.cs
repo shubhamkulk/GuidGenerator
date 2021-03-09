2d8b57efaa3aef1aba1ce8c6a79d06454c5e4a80
Shubham KulkarniShubham Kulkarni <shubhamkulk4@gmail.com>
2021-03-09 14:44:15 +0530

    Add project files.

---
+++ GuidGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuidGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
